5c9585949dd2d4981cc7e452656b53c54d4ec232
Wim Van Dijck <github@sinoid.be>
2012-12-25 14:08:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Books simple bookkeeping application
+
+This is a simple application to keep track of bills, invoices, and customers.
+It is made partially for a personal need and as an education for myself.
+
+So don't expect too much of it :)
+[Wim](http://www.sinoid.be)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebooks::Application.config.secret_token = '0e337bcdcc7c5b6f52945788bd2f8b9a21e8f51875412707c36d34c0177ac055aee12aee62d0859f651a2c0f96711f65aab8f9bc70dc1ff25811516f6bf9c75c'
