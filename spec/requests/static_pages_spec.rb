0e8b69590c5c73f40e62379c06ff8bee011bda8a
Wim Van Dijck <github@sinoid.be>
2012-12-27 21:57:49 +0100

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,45 +1,48 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
+describe "Static Pages" do
 
-  	describe "Home Page" do
+	describe "Home Page" do
 
-  		it "should have the h1 'The Books'" do
-  			visit '/static_pages/home'
-  			page.should have_selector('h1', :text => "The Books")
-  		end
+		it "should have the h1 'The Books'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => "The Books")
+		end
 
-  		it "should have the right title" do
-  		  visit '/static_pages/home'
-  		  page.should have_selector('title', :text => "The Books | Home")
-  		end
-  	end
+		it "should have the right title" do
+		  visit '/static_pages/home'
+		  page.should have_selector('title', :text => "The Books")
+		end
 
-  	describe "Help Page" do
+    it "should not have a custom title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+	end
 
-  		it "should have the h1 'Help'" do
-  			visit '/static_pages/help'
-  			page.should have_selector('h1', :text => 'Help')
-  		end
+	describe "Help Page" do
 
-      it "should have the right title" do
-        visit '/static_pages/help'
-        page.should have_selector('title', :text => 'The Books | Help')        
-      end
-  	end
+		it "should have the h1 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		end
 
-  	describe "About Page" do
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "The Books")        
+    end
+	end
 
-  		it "should have the h1 'About'" do
-  			visit '/static_pages/about'
-  			page.should have_selector('h1', :text => 'About')
-  		end
+	describe "About Page" do
 
-      it "should have the right title" do
-        visit '/static_pages/about'
-        page.should have_selector('title', :text => 'The Books | About')
-      end
-  	end
-  end
+		it "should have the h1 'About'" do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About')
+		end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "The Books")
+    end
+	end
 end
